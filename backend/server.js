a52b35c566d117a78d532d9afcf2d61c13b4278d
HurasAdam <huradam@wp.pl>
2023-05-26 21:42:58 +0200

    Added basic server config

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const http= require('http');
+const cors = require('cors')
 const server = http.createServer(app);
 const {Server}=require('socket.io');
 const io = new Server(server,{
     cors:'*',
     method:'*'
+})
+
+app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+server.listen(4000,()=>{
+    console.log('Server running on port, 4000')
 })
