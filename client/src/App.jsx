fa0aa16ba20ab770ba1eec77ca7f09d984fea714
HurasAdam <huradam@wp.pl>
2023-05-26 23:22:31 +0200

    Added Navigation, Home page, set Home as index route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 
 import './App.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navigation} from './components/Navigation'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {Home} from './components/pages/Home'
 function App() {
 
   return (
-    <>
-     
-    </>
+ <div className="App">
+<BrowserRouter>
+<Navigation>
+  <Routes>
+<Route index element={<Home/>}/>
+  </Routes>
+</Navigation>
+</BrowserRouter>
+ </div>
   )
 }
 
