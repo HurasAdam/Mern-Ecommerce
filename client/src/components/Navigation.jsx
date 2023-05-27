6646b19fbeedabf082047a72c24b872f06dcaa58
HurasAdam <huradam@wp.pl>
2023-05-27 23:42:33 +0200

    Added env variables, db connection config, User Model, user routes and basic controllers

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -13,7 +13,7 @@ export const Navigation = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
 
-            <Nav.Link href="/login">Home</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
