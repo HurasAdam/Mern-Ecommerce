dbab946d9aa268850f4a66194e40eec3d5513aa2
HurasAdam <huradam@wp.pl>
2023-07-07 23:58:42 +0200

    Added CartPage product list with details : product image, price, quanity and active buttons to increase and decrease quanity, Added logic to increase and decrease quanity of produts in cart

--- client/src/components/pages/CartPage.jsx
+++ client/src/components/pages/CartPage.jsx
@@ -1,23 +1,85 @@
-import './CartPage.css'
-import { useSelector } from 'react-redux'
-import { Container,Row,Alert } from 'react-bootstrap'
+import "./CartPage.css";
+import { useSelector } from "react-redux";
+import { Container, Row, Alert, Col, Table } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faCircleMinus } from "@fortawesome/free-solid-svg-icons";
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import { useIncreaseCartProductMutation,useDecreaseCartProductMutation,useRemoveFromCartMutation } from "../../services/appApi";
 function CartPage() {
-const user = useSelector(state=>state.user)
-const products = useSelector(state=>state.products)
-const userCartObj= user.cart;
-let cart = products.filter((product)=>userCartObj[product._id]!=null);
-  return (
-<Container style={{minHeight:"95vh"}} className='cart-container'>
-    <Row>
-        <h1 className='pt-2 h3'>Shopping cart</h1>
-        {cart.length==0?(
-            <Alert variant='info'>Shopping cart is empty. Add products to your cart</Alert>
-        ):(<div>Payment here</div>)
-        }
-    </Row>
+  const user = useSelector((state) => state.user);
+  const products = useSelector((state) => state.products);
+  const userCartObj = user.cart;
+  let cart = products.filter((product) => userCartObj[product._id] != null);
+  const [increaseCart]=useIncreaseCartProductMutation();
+  const [decreaseCart]=useDecreaseCartProductMutation();
+  const [removeFromCart]=useRemoveFromCartMutation();
 
-</Container>
-  )
+
+  function handleDecrease(e,product){
+    const quantity = user.cart.count;
+    if(quantity<=0) return alert("Cant proceed")
+decreaseCart(product)
+  }
+  return (
+    <Container style={{ minHeight: "95vh" }} className="cart-container">
+      <Row>
+        <Col md={7}>
+          <h1 className="pt-2 h3">Shopping cart</h1>
+          {cart.length == 0 ? (
+            <Alert variant="info">
+              Shopping cart is empty. Add products to your cart
+            </Alert>
+          ) : (
+            <div>Payment here</div>
+          )}
+        </Col>
+        <Col md={5}>
+          {cart.length > 0 && (
+            <>
+              <Table responsive="sm" className="cart-table">
+                <thead>
+                  <tr>
+                    <th>&nbsp;</th>
+                    <th>Product</th>
+                    <th>Price</th>
+                    <th>Quanity</th>
+                    <th>Subtotal</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* LOOP thru cart products */}
+                  {cart.map((item) => {
+                    return (
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td>
+                          <FontAwesomeIcon className="cart-action-icon remove" icon={faXmark} style={{marginRight:10}}  />
+                          <img src={item.pictures[0].url} style={{width:100, height:100,objectFit:"cover"}} alt="" />
+                        </td>
+                        <td>{item.price}</td>
+                        <td>
+                          <span className="quantity-indicator">
+                          <FontAwesomeIcon className="cart-action-icon plus"  icon={faCircleMinus} size="lg" style={{color: "#27282b",}} onClick={()=>increaseCart({productId:item._id,price:item.price,userId:user._id})}/>
+                          <span>{user.cart[item._id]}</span>
+                          <FontAwesomeIcon className="cart-action-icon minus"  icon={faCirclePlus} size="lg" style={{color: "#27282b",}} onClick={(e)=>decreaseCart({productId:item._id,price:item.price,userId:user._id})} />
+                          </span>
+                        </td>
+                        <td>{item.price * user.cart[item._id]}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
+              <div>
+                <h3 className="h4 pt-4">Total:${user.cart.total}</h3>
+              </div>
+            </>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default CartPage
+export default CartPage;
