e07efae24bb2f78d7c01dea482305540dd96ff7c
HurasAdam <huradam@wp.pl>
2023-07-23 20:31:59 +0200

    Added function to handle showOrder and display order details in Modal

--- client/src/components/OrdersAdminPage.jsx
+++ client/src/components/OrdersAdminPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import axios from "../axios";
 import Loading from "../components/Loading";
-import { Badge, Button, Table } from "react-bootstrap";
+import { Badge, Button, Table, Modal } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 function OrdersAdminPage() {
@@ -12,13 +12,23 @@ function OrdersAdminPage() {
   const [orderToShow, setORderToShow] = useState([]);
   const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
   function markShipped(orderId, ownerId) {
-axios.patch(`/orders/${orderId}/mark-shipped`,{ownerId})
-.then(({data})=>setOrders(data))
-.catch((e)=>console.log(e))
+    axios
+      .patch(`/orders/${orderId}/mark-shipped`, { ownerId })
+      .then(({ data }) => setOrders(data))
+      .catch((e) => console.log(e));
   }
-  function showOrder(){
-
+  function showOrder(productsObj) {
+    let productsToShow = products.filter((product) => productsObj[product._id]);
+    productsToShow = productsToShow.map((product) => {
+      const productCopy = { ...product };
+      productCopy.count = productsObj[product._id];
+      delete productCopy.description;
+      return productCopy;
+    });
+    setShow(true);
+    setORderToShow(productsToShow);
   }
 
   useEffect(() => {
@@ -44,48 +54,77 @@ axios.patch(`/orders/${orderId}/mark-shipped`,{ownerId})
     return <h1 className="text-center pt-4">No orders yet</h1>;
   }
   return (
-    <Table responsive striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Client Name</th>
-          <th>Items</th>
-          <th>Order Total</th>
-          <th>Adress</th>
-        </tr>
-      </thead>
-      <tbody>
-        {orders.map((order) => {
-          return (
-            <tr>
-              <td>{order._id}</td>
-              <td>{order?.owner?.name}</td>
-              <td>{order.count}</td>
-              <td>${order.total}</td>
-              <td>{order.adress}</td>
-              <td>
-                {order.status === "processing" ? (
-                  <Button
-                    size="sm"
-                    onClick={() => markShipped(order._id, order.owner?._id)}
+    <>
+      <Table responsive striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Client Name</th>
+            <th>Items</th>
+            <th>Order Total</th>
+            <th>Adress</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order) => {
+            return (
+              <tr>
+                <td>{order._id}</td>
+                <td>{order?.owner?.name}</td>
+                <td>{order.count}</td>
+                <td>${order.total}</td>
+                <td>{order.adress}</td>
+                <td>
+                  {order.status === "processing" ? (
+                    <Button
+                      size="sm"
+                      onClick={() => markShipped(order._id, order.owner?._id)}
+                    >
+                      Mark as shhipped
+                    </Button>
+                  ) : (
+                    <Badge bg="success">Shipped</Badge>
+                  )}
+                </td>
+                <td>
+                  <span
+                    style={{ cursor: "pointer" }}
+                    onClick={() => showOrder(products)}
                   >
-                    Mark as shhipped
-                  </Button>
-                ) : (
-                  <Badge bg="success">Shipped</Badge>
-                )}
-              </td>
-              <td>
-                <span style={{ cursor: "pointer" }} onClick={()=>showOrder(products)}>
-                  View order
-                  <FontAwesomeIcon icon={faEye} style={{margin:"0px 6px"}}/>
-                </span>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+                    View order
+                    <FontAwesomeIcon
+                      icon={faEye}
+                      style={{ margin: "0px 6px" }}
+                    />
+                  </span>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Order details</Modal.Title>
+          {orderToShow.map((order) => {
+            return (
+              <div className="order-details__container">
+                <img
+                  src={order.pictures[0].url}
+                  alt=""
+                  style={{ maxWidth: 100, height: 100, objectFit: "cover" }}
+                />
+                <p>
+                  <span>{order.count} x</span>
+                  {order.name}
+                  <p>Price ${Number(order.price)*product.count}</p>
+                </p>
+              </div>
+            );
+          })}
+        </Modal.Header>
+      </Modal>
+    </>
   );
 }
 export default OrdersAdminPage;
